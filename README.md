21c8f1170df63a798fe99ba7f9a8ac9ffc18a109
nathancarroll <ntc238@gmail.com>
2021-11-21 23:08:30 -0600

    refactor main.js to use categoryObject class, update README

--- README.md
+++ README.md
@@ -14,13 +14,11 @@ If you do not have Node installed, you can download it and find more information
 
 I chose javascript for this program due to its flexibility and ease of use. In a small application, a scripting language like JS makes it easy to iterate, and the total volume of code is minimal enough that any errors are easy to trace through the stack. In a larger, more robust application, I would probably choose to use C# on the back-end.
 
-The basic structure of the program is dictated by the asynchronous nature of the readline module. Each successive step of soliciting user input needs to be called from within the callback function of the previous step. Since there are only three steps, I made a function for each of them, and the rest of the code is basically just helper methods and text string manipulation.
- 
-If there were more steps, it would make sense to abstract/generalize the functions for getting user input. You could have a class to represent a generic step, then the traits and methods shared by each step could be inherited from that common class, and any individual features could be coded exactly as needed.
+The main class within the program is called categoryObject, and the code instantiates an object for each of the steps where user input is needed: guest, hotel, and template. This design is meant to make it easier to add more steps as required in the future. Ideally the function that prompts for user input would also be included within the categoryObject class, but the asynchronous nature of the console readline module makes that tricky to implement, as the function for soliciting the next step of input needs to be included within the callback function of the previous step's readline call. In a GUI where users are presented with all options at once rather than sequentially, including a getInput function in the class would make much more sense.
 
 ### Verification
 
-I verified this program through manual testing on my local machine. I used a variety of different inputs, and observed that the program does not proceed unless a number representing a valid option is input; this is the expected behavior.
+I verified this program through manual testing on my local machine. I used a variety of different inputs, and observed that the program does not proceed unless a number representing a valid option is input; this is the expected behavior. There are some drawbacks to the way placeholder names are used in the templates; the placeholder names themselves are essentially off limits for use in the greeting message, as the code will go ahead and replace them with values. You could get around this by prepending them with some sort of symbol and changing the keys of the replacements object appropriately, but that might sacrifice a bit of user-friendliness with regard to the custom message templates. 
 
 ### Next Steps
 
--- main.js
+++ main.js
@@ -5,44 +5,54 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-// Parse each of the json files to an array of objects.
-// I'm not using any error handling here since the files are static in this little application, but in real-world
-// use cases, you would want to handle any errors that arise when parsing from text files like this.
-const guests = require('./Guests.json');
-const hotels = require('./Companies.json');
-const templates = require('./Templates.json');
-
 const prompts = {
     selectHotel: "===== ENTER A NUMBER TO SELECT A HOTEL =====\n",
     selectGuest: "===== ENTER A NUMBER TO SELECT A GUEST =====\n",
     selectTemplate: "===== ENTER A NUMBER TO SELECT A GREETING =====\n",
     createTemplate: "===== ENTER A CUSTOM GREETING =====\n"
+}
+
+// Parse each of the json files to an array of objects.
+// I'm not using any error handling here since the files are static in this little application, but in real-world
+// use cases, you would want to handle any errors that arise when parsing from text files like this.
+const filepaths = {
+    guest: './Guests.json',
+    hotel: './Companies.json',
+    template: './Templates.json'
 };
 
-// Returns a string for display to the user with numbered options for guest, hotel, or greeting
-function getSelectText(category){
-    let text;
-    if (category === 'guest'){
-        text = prompts.selectGuest;
-        guests.forEach(guest => {
-            text += `${guest.id}: ${guest.firstName} ${guest.lastName} \n`; 
-        })        
-    } else if (category === 'hotel'){
-        text = prompts.selectHotel;
-        hotels.forEach(hotel => {
-            text += `${hotel.id}: ${hotel.company} \n`; 
-        })
-    } else if (category === 'template'){
-        text = prompts.selectTemplate
-        text += "0: Custom Greeting \n";
-        templates.forEach(template => {
-            text += `${template.id}: ${template.text} \n`; 
-        });
-    };  
-    return text;
+// This class is used to create the properties for the guest, hotel, and template objects.
+// Abstracting into a class will make it easier to add categories in the future if additional inputs are added to the template format.
+class categoryObject {
+    constructor(categoryName){
+        this.categoryName = categoryName;
+        this.items = require(filepaths[categoryName]);
+        this.selectMessage = (function(categoryName, items){
+            let text;
+            if (categoryName === 'guest'){
+                text = prompts.selectGuest;
+                items.forEach(guest => {
+                    text += `${guest.id}: ${guest.firstName} ${guest.lastName} \n`; 
+                })        
+            } else if (categoryName === 'hotel'){
+                text = prompts.selectHotel;
+                items.forEach(hotel => {
+                    text += `${hotel.id}: ${hotel.company} \n`; 
+                })
+            } else if (categoryName === 'template'){
+                text = prompts.selectTemplate
+                text += "0: Custom Greeting \n";
+                items.forEach(template => {
+                    text += `${template.id}: ${template.text} \n`; 
+                });
+            };
+            return text;
+        })(categoryName, this.items);
+        this.errorMessage = `${categoryName.charAt(0).toUpperCase() + categoryName.slice(1)} not found! Please try again.\n`
+    };
 };
 
-// Returns the word referencing the time of day based on the current hour
+// Returns the word referencing the time of day based on the current hour.
 function getTimeOfDay(){
     let timeOfDay;
     let hour = new Date().getHours();
@@ -58,31 +68,34 @@ function getTimeOfDay(){
     return timeOfDay;
 };
 
-// This helper function takes in a timestamp of the form seen in the guests.json file and returns that time in a more readable format
+// This helper function takes in a timestamp of the form seen in the guests.json file and returns that time in a more readable format.
 function formatTimestamp(timestamp) {
     let d = new Date(timestamp);
     return d.toLocaleDateString('en-US') + ' ' + d.toLocaleTimeString('en-US');
 };
 
 let guest, hotel, template; // These global variables are set by the user input from within the readline questions
+let guestObject = new categoryObject('guest');
+let hotelObject = new categoryObject('hotel');
+let templateObject = new categoryObject('template');
 
 // getGuest, getHotel, and getTemplate are stepped through in order, getting input from the user at each step
 function getGuest(){
-    rl.question(getSelectText('guest'), function(res){
-        guest = guests.find(guest => guest.id == res);
-        guest ? getHotel() : handleInputError('guest'); // If guest is non-null, advance to the next step
+    rl.question(guestObject.selectMessage, function(res){
+        guest = guestObject.items.find(guest => guest.id == res);
+        guest ? getHotel() : handleInputError(guestObject.errorMessage, getGuest); // If guest is non-null, advance to the next step
     });
 };
 
 function getHotel(){
-    rl.question(getSelectText('hotel'), function(res){
-        hotel = hotels.find(hotel => hotel.id == res);
-        hotel ? getTemplate() : handleInputError('hotel'); // If hotel is non-null, advance to the next step
+    rl.question(hotelObject.selectMessage, function(res){
+        hotel = hotelObject.items.find(hotel => hotel.id == res);
+        hotel ? getTemplate() : handleInputError(hotelObject.errorMessage, getHotel); // If hotel is non-null, advance to the next step
     });
 };
 
 function getTemplate(){
-    rl.question(getSelectText('template'), function(res){
+    rl.question(templateObject.selectMessage, function(res){
         if (res == 0){  // Custom greeting template 
             rl.question(prompts.createTemplate, function(res){
                 template = {
@@ -93,24 +106,15 @@ function getTemplate(){
                 greet();
             });
         } else { // Pre-existing greeting template
-            template = templates.find(template => template.id == res);
-            template ? greet() : handleInputError('template'); // Once guest, hotel, and template are all non-null, call the greet function
+            template = templateObject.items.find(template => template.id == res);
+            template ? greet() : handleInputError(templateObject.errorMessage, getTemplate); // Once guest, hotel, and template are all non-null, call the greet function
         };
     });
 };
 
-// Provides feedback to the user and restarts whichever step of the process just failed
-function handleInputError(category){
-    if (category === 'guest'){
-        console.log('Guest not found! Please try again. \n');
-        getGuest();        
-    } else if (category === 'hotel'){
-        console.log('Hotel not found! Please try again. \n');
-        getHotel();
-    } else if (category === 'template'){
-        console.log('Template not found! Please try again. \n');
-        getTemplate();
-    };
+function handleInputError(message, callback){
+    console.log(message);
+    callback();
 };
 
 // Prints the greeting to the console
@@ -138,5 +142,4 @@ function greet(){
     rl.close(); // Close the readline process so the program terminates instead of perpetually awaiting input from the console
 };
 
-// The initial call to getGuest sets the program in motion
 getGuest();
